164ba78d63e4fa3c2be1c657cfd33ccc830e620c
AFWebDev <andrewfariaj@gmail.com>
2019-06-27 13:33:50 -0400

    add images, and player select screen

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    //Define a charcters array of Objects with player character info
+    const characters = [
+        {
+            name: "Finn",
+            health: 100,
+            attack: 9
+        },
+        {
+            name: "Jake",
+            health: 95,
+            attack: 10
+        },
+        {
+            name: "BMO",
+            health: 50,
+            attack: 5
+        },
+        {
+            name: "Marceline",
+            health: 70,
+            attack: 10
+        },
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
