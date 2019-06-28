89aab28653e5d0f57e384ba6c318488aedbbd805
AFWebDev <andrewfariaj@gmail.com>
2019-06-28 11:11:19 -0400

    new fns -> reduce dry code. lots of comments, fns to create elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    let gameStarted = false;
     let chosenPlayer;
     let chosenEnemy;
 
 
-    //Define a charcters array of Objects with character info
+    //Define an array of PLAYER objects
     const characters = [
         {
-            name: "finn",
+            name: "Finn",
             health: 100,
             attack: 9,
             image: '/assets/images/characters/player/Finn_with_bionic_arm-0.png',
             enemy: false
         },
         {
-            name: "jake",
+            name: "Jake",
             health: 95,
             attack: 10,
             image: '/assets/images/characters/player/JaketheDog.png',
             enemy: false
         },
         {
-            name: "bmo",
+            name: "BMO",
             health: 50,
             attack: 5,
             image: '/assets/images/characters/player/bmo.png',
             enemy: false
         },
         {
-            name: "marceline",
+            name: "Marceline",
             health: 70,
             attack: 10,
             image: '/assets/images/characters/player/marceline.png',
@@ -37,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     ]
 
+    //Define an array of ENEMY objects
     const enemies = [
         {
             name: "Ice King",
@@ -67,11 +67,13 @@ document.addEventListener('DOMContentLoaded', function () {
             $('.characters').append(buildCharcterCard(character))
         })
     }
-
+    //Method 1
+    //Written HTML to be used inside append.
+    //This Function takes an object, and uses its values to return dynamic info/html template
     const buildCharcterCard = function (character) {
         return (`
-            <div class="character">
-                <div class="portrait">
+        <div class="character">
+            <div class="portrait">
                 <img class="charSelectImage" data-name="${character.name}" data-enemy="${character.enemy}"
                 src="${character.image}" alt="${character.name}">
             </div>
@@ -80,108 +82,100 @@ document.addEventListener('DOMContentLoaded', function () {
                 <p class="health mt-2">Health: <span>${character.health}</span></p>
                 <p class="attack mt-1">Attack: <span>${character.attack}</span></p>
             </div>
-            </div>`)
+        </div>`)
     }
 
+    //Method 2
+    //HTML built using jquery created elements, it creates the same div for player and enemy,
+    //This function takes in two objects. It contains an inner function named builder.
+    //This fn is used to append info based on chosen player/enemy sent to this fn as player, enemy
+    //builder is used to return pre-built structure of elements.
+    const buildBattleScreen = function (player, enemy) {
+        console.log(this)
+        //Element builder
+        const builder = function (character) {
+            let img = $('<img>').addClass('battleImage').attr({
+                'data-name': character.name,
+                src: character.image,
+                alt: character.name
+            })
+            let infoName = $('<p>').text(`Name: ${character.name}`)
+            let infoHP = $('<p>').text(`Health: ${character.health}`)
+            let infoAttack = $('<p>').text(`Attack: ${character.attack}`)
+
+            let charDiv = $('<div>').addClass('character')
+            let portraitDiv = $('<div>').addClass('portrait').append(img)
+            let infoDiv = $('<div>').addClass('info').append(infoName, infoHP, infoAttack)
+
+            return charDiv.append(portraitDiv.append(infoDiv));
+        }
+
+        //OUR CHARACTERS NOW DRAWN ON THE DOM.
+        $('.characters').append(builder(player))
+            .append('<h1>FIGHT</h1>')
+            .append(builder(enemy))
+    }
+
+
+    //This function beginBattle is called after enemy is chosen,
+    //it appends info returned from buildBattleScreen after recieving both player and enemy objs.
+    const beginBattle = function (player, enemy) {
+        $('.characters').append(buildBattleScreen(player, enemy))
+    }
+
+    //END FUNCTIONS AND VARIABLE DECLARATION.
+
+
+
+
+
+    //INIT GAME
 
     //Init game. Build character elements based on array using a function buildCharacter
     buildCharacter(characters);
     //Char select screen is now created, and displayed.
 
 
-    // on click handlers here, it should look at enemy type ?? not sure stuck here.
-    //the attribute of .charSelectImage is one time(during selection phases) and changed during battle?
-    $('.characters').on('click', '.charSelectImage', function (e) {
-        // console.log(this)
-        // console.log(e)
-
+    // on click handler, it should look at enemy data-type=T/F (string)
+    //the class of .charSelectImage is one time(during selection phases) and changed during battle,
+    //thus, when in battle this onclick is not used.
+    $('.characters').on('click', '.charSelectImage', function () {
+        //uses dataset info to decide which array to iterate, and to assign that character obj,
+        //to its defined, -> chosenPlayer or chosenEnemy
         let isEnemy = this.dataset.enemy;
         let name = this.dataset.name;
 
-        //if true, its an enemy,
-        //CLEAR THE SCREEN, STORE THE ENEMEY, COMMENCE THE BATTLE.
+        //An enemy was clicked at char select screen, we go to battle now!
+        //CLEAR THE SCREEN, STORE THE ENEMY, COMMENCE THE BATTLE.
         if (isEnemy === "true") {
-            $('.characters').html('')
+            //CLEAR THE SCREEN
+            $('.characters').html('');
+
+            //STORE ENEMY
             enemies.forEach(function (enemy) {
                 if (name === enemy.name) {
                     chosenEnemy = enemy;
                 }
             })
+
             //Commence battle with both enemy and player selected character.
-            //some function() call to begin battle here
+            //beginBattle(player, enemy)
+            beginBattle(chosenPlayer, chosenEnemy)
 
-
-            //ELSE, CLEAR THE SCREEN, SHOW CHARCTER SELECT SCREEN.
+            //PLAYER CHARACTER WAS CHOSEN(Clicked)
+            //ELSE, CLEAR THE SCREEN, STORE SELECTED CHARACTER, SHOW ENEMY CHARCTER SELECT SCREEN.
         } else {
+            //CLEAR THE SCREEN
             $('.characters').html('')
-            console.log('is not an enemy')
+            //STORE THE PLAYER SELECTED CHARACTER
+            characters.forEach(function (character) {
+                if (name === character.name) {
+                    chosenPlayer = character;
+                }
+            })
+            //SHOW ENEMY SELECT SCREEN
+            buildCharacter(enemies);
         }
     })
-
-
-
-    // $('.characters').on('click', '.charSelectImage', function (event) {
-
-    //     let playerChosenName = this.dataset.name;
-    //     characters.forEach(function (i) {
-    //         if (playerChosenName === i.name) {
-    //             chosenPlayer = i;
-    //         }
-    //     })
-    //     // make a call to function to set up a game here.
-    //     //the function should clear screen, build a new one with enemy array.
-    //     gameSetup(chosenPlayer)
-    // })
-
-
-
-
-
-    //NOTHING HERE CURRENTLY IN USE, KEEPING FOR REF.
-
-    const gameSetup = function (player) {
-        //Remove (player select screen)
-        $('.characters').html('')
-        //Change instruction message
-        $('#instruction').text('Choose An Enemy!')
-
-        //Store a new Character Element for chosen character
-        let characterElement = $(`
-            <div class="character">
-                <div class="portrait">
-                    <img class="player" data-name="${player.name}"
-                    src="${player.image}" alt="${player.name}">
-                </div>
-                <div class="info">
-                    <p class="name mt-1">Name: <span>${player.name}</span></p>
-                    <p class="health mt-2">Health: <span>${player.health}</span></p>
-                    <p class="attack mt-1">Attack: <span>${player.attack}</span></p>
-                </div>
-            </div>`)
-
-
-
-
-
-    }//Gamesetup function END
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
